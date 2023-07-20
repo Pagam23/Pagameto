dc767828644ddcda54b66953abc2106dcf5d14db
Pagam23 <140122240+Pagam23@users.noreply.github.com>
2023-07-20 19:47:04 -0300

    Update and rename Pagamento to Pagamento.php

---
+++ Pagamento.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Página de Pagamento Feliz - Processamento</title>
+  <style>
+    /* Estilos CSS permanecem inalterados */
+    /* ... */
+  </style>
+</head>
+<body>
+  <div class="payment-form">
+    <h1>Pagamento Processado!</h1>
+    <p>O pagamento de <?php echo $_POST['NomeCompleto']; ?> foi processado com sucesso.</p>
+    <!-- Substitua "NomeCompleto" pelos nomes dos outros campos do formulário conforme necessário -->
+    <p>Pagamentos felizes trazem sorrisos!</p>
+  </div>
+</body>
+</html>
